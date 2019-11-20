7df38517797151814baec940d393b65b90953ffa
Victoria Hale <vic.hale74@gmail.com>
2019-11-20 17:27:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-
+  get '/' do 
+    erb :'views/super_hero'
+  end
+  
+  post '/views/teams' do 
+    
+  end
 
 end
