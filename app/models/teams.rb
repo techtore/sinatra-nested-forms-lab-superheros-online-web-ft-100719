ac2a028da70f5ad75d41f437f448c8c40ff5e729
Victoria Hale <vic.hale74@gmail.com>
2019-11-20 18:19:19 +0000

    Automatically backed up by Learn

--- app/models/teams.rb
+++ app/models/teams.rb
@@ -1,14 +1,10 @@
 class Team 
   attr_reader :name, :motto
-  TEAMS = []
   
   def initialize(params)
     @name = params[name]
     @motto = params[motto]
     TEAMS << self
   end
-  
-  def self.all 
-    TEAMS
-  end
+
 end
