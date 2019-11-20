95c25f18991b9348c177c3cf30e5d9b55e4dc168
Victoria Hale <vic.hale74@gmail.com>
2019-11-20 18:04:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
     
   get '/' do 
-    erb :'super_hero'
+    erb :super_hero
   end
   
   post '/teams' do 
@@ -16,7 +16,7 @@ class App < Sinatra::Base
       end
       @heroes = Hero.all
       
-      erb :'/team'
+      erb :team
   end
 
 end
